cc6bed809dbb3615187f5d4751779e8761e6010f
Zanggar <zmbakh@gmail.com>
2022-02-13 14:50:17 +0600

    Food entry store, seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,9 +19,16 @@ class DatabaseSeeder extends Seeder
         $users = User::get();
 
         foreach ($users as $user) {
-            FoodEntry::factory()->count(1000)->create([
-                'user_id' => $user->id,
-            ]);
+            $date = now();
+            for ($i = 0; $i < 40; $i++) {
+                for ($k = 0; $k < 10; $k++) {
+                    FoodEntry::factory()->create([
+                        'user_id' => $user->id,
+                        'date_time' => $date->toDateString(). ' '. mt_rand(0, 23) . ':' . mt_rand(0, 59),
+                    ]);
+                }
+                $date->subDay();
+            }
         }
     }
 }
