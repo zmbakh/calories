1ced0aa29ebcc68991a3cbed20fbe446acf03ffb
Zanggar <zmbakh@gmail.com>
2022-02-15 23:06:02 +0600

    Frontend: admin reports

--- resources/vue/views/Pages/Login.vue
+++ resources/vue/views/Pages/Login.vue
@@ -53,7 +53,7 @@
           </b-card>
           <b-row class="mt-3">
             <b-col cols="6">
-              <router-link to="/dashboard" class="text-light"><small>Forgot password?</small></router-link>
+                
             </b-col>
             <b-col cols="6" class="text-right">
               <router-link to="/register" class="text-light"><small>Create new account</small></router-link>
